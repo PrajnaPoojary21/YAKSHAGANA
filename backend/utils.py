d855083fd8ae7f958d8e86f8806c2df8df9bdf76
PrajnaPoojary21 <prajnag603@gmail.com>
2026-04-26 10:25:39 +0530

    Updated backend after merging frontend

--- backend/utils.py
+++ backend/utils.py
@@ -1,7 +1,13 @@
-
 import re
 
 def clean_text(text):
-    # remove repeated characters
-    cleaned = re.sub(r'(.)\1+', r'\1', text)
-    return cleaned
+    # remove long repeated characters
+    text = re.sub(r'(.)\1{2,}', r'\1', text)
+
+    # remove non-Kannada
+    text = re.sub(r'[^\u0C80-\u0CFF\s]', '', text)
+
+    # remove extra spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
