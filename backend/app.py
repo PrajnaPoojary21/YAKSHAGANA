19945f39a54ce6540dc94ec44d9cf4650c1ca738
PrajnaPoojary21 <prajnag603@gmail.com>
2026-04-27 16:49:13 +0530

    modified frontend-chnaged basic components

--- backend/app.py
+++ backend/app.py
@@ -40,3 +40,50 @@ def transcribe():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import os
+# import uuid
+# from whisper_model import transcribe_audio
+# from utils import clean_text
+
+# app = Flask(__name__)
+# CORS(app)
+
+# UPLOAD_FOLDER = "audio_temp"
+# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# @app.route("/")
+# def home():
+#     return "Yakshagana Backend Running!"
+
+# @app.route("/upload", methods=["POST"])
+# def upload():
+#     if "audio" not in request.files:
+#         return jsonify({"error": "No file uploaded"}), 400
+
+#     file = request.files["audio"]
+#     file_path = os.path.join(UPLOAD_FOLDER, str(uuid.uuid4()) + ".wav")
+
+#     file.save(file_path)
+
+#     try:
+#         raw_text = transcribe_audio(file_path)
+#         cleaned_text = clean_text(raw_text)
+
+#         return jsonify({
+#             "text": cleaned_text
+#         })
+
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
+#     finally:
+#         if os.path.exists(file_path):
+#             os.remove(file_path)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
